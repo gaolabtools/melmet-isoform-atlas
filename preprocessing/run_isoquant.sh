0545e433335e4efab08d07d7c15742d83f214c33
Lina Lu <93294093+linalu1121@users.noreply.github.com>
2026-04-28 22:22:02 -0500

    Isoform-level quantification

---
+++ preprocessing/run_isoquant.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# ================================
+# Isoform-level quantification
+# Software: IsoQuant v3.3.1
+
+# Purpose:
+# Detect novel transcript isoforms and quantify isoform expression in individual cells 
+# ================================
+
+
+# Activate isoquant environment
+conda activate /home/software/miniconda3/envs/isoquant
+
+# Run isoquant pipeline
+isoquant.py \
+--data_type nanopore \
+--bam_list bam_file_list.txt \
+--read_group tag:CB \
+--genedb /home/genome/refdata-gex-GRCh38-2024-A/genes/genes.gtf \
+--complete_genedb \
+--reference /home/genome/refdata-gex-GRCh38-2024-A/fasta/genome.fa \
+--output allsamples \
+--prefix allsamples \
+--threads 20 \
+--clean_start
